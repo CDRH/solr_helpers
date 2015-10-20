95c1440020aec6fe77ab95e5bb45d953f63dd2ef
Jessica Dussault <jdussault4@gmail.com>
2015-10-20 15:42:20 -0500

    Paginator doesn't is now a standalone pagination returner (used to be partially in a template)

--- lib/solr_helpers/pagination.rb
+++ lib/solr_helpers/pagination.rb
@@ -15,13 +15,29 @@ module SolrHelpers::Pagination
     return html
   end
 
+  # paginator
+  #   I am so surprised that this is working
+  #   params: total_pages (int 12), display_range (int 3), params (rails object)
+  #   return: giant blob of html like << 1 ... 4, 5, 6 ... 12 >>
+  def paginator(total_pages, display_range=3, aParams=params)
+    if total_pages && total_pages > 1
+      current_page = aParams["page"] ? aParams["page"].to_i : 1
+      html = "<nav><ul class='pagination'>"
+      html += page_button_previous(current_page, aParams)
+      html += paginator_numbers(total_pages, display_range, aParams)
+      html += page_button_next(current_page, total_pages, aParams)
+      html += "</ul></nav>"
+      return html.html_safe
+    end
+  end
+
   # paginator_numbers
   #   params: total_pages (12)
   #           display_range (default is 3, determines how many buttons appear in pagination on each side of current page)
   #           params (rails params or hash)
   #   returns: big html snippet that has been deemed html_safe, however unwisely
   #           contains first, current, and last pages in search results
-  #           as well as padding the current page with the previous and next pages
+  #           and several numbers on each side of the current (display_range)
   def paginator_numbers(total_pages, display_range=3, aParams=params)
     current_page = aParams[:page] ? aParams[:page].to_i : 1
     total_pages = total_pages.to_i
@@ -54,6 +70,37 @@ module SolrHelpers::Pagination
     end
 
     return html.html_safe
+  end
+
+  # page_button_previous
+  #   params: current_page (2), rails params
+  #   returns: link to previous page with button <<
+  def page_button_previous(current_page, aParams)
+    html = ""
+    html += (current_page == 1) ? "<li class='disabled'>" : "<li>"
+    if current_page != 1
+      html += link_to "<span aria-hidden='true'>&laquo;</span>".html_safe, to_page(current_page-1, aParams)
+    else
+      # use a span instead of a link if it is inactive
+      html += "<span><span aria-hidden='true'>&laquo;</span></span>"
+    end
+    html += "</li>"
+    return html
+  end
+
+  # page_button_next
+  #   params: current_page (13), total_pages (15), rails params
+  #   returns: link to next page with button >>
+  def page_button_next(current_page, total_pages, aParams)
+    html = ""
+    html += (current_page == total_pages) ? "<li class='disabled'>" : "<li>"
+    if current_page != total_pages
+      html += link_to "<span aria-hidden='true'>&raquo;</span>".html_safe, to_page(current_page+1, aParams)
+    else
+      html += "<span><span aria-hidden='true'>&raquo;</span></span>"
+    end
+    html += "</li>"
+    return html
   end
 
   # result_text
--- lib/solr_helpers/railtie.rb
+++ lib/solr_helpers/railtie.rb
@@ -7,8 +7,10 @@ module SolrHelpers
         include SolrHelpers::Sorting
       end
 
+      # This is probably not in the spirit of this view oriented gem but 
+      # I want to keep the comment so I can find how to do it in the future
       # ActiveSupport.on_load :action_controller do
-      #   include CdrhPagination::SomethingForControllers
+      #   include SolrHelpers::SomethingForControllers
       # end
     end
   end
